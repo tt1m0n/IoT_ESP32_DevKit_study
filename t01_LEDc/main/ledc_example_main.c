5dd8cf127f3475052cd48fe0d9aff17d65d69457
makovskyi <makovskiy.kpd@gmail.com>
2020-08-13 12:26:17 +0300

    replaced tabs to spaces in t01

--- t01_LEDc/main/ledc_example_main.c
+++ t01_LEDc/main/ledc_example_main.c
@@ -6,11 +6,11 @@
 
 void app_main()
 {
-	ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_26, HIGH_LEVEL));
-	ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_27, HIGH_LEVEL));
-	ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_33, HIGH_LEVEL));
+    ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_26, HIGH_LEVEL));
+    ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_27, HIGH_LEVEL));
+    ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_33, HIGH_LEVEL));
 
-	ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT));
-	ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_27, GPIO_MODE_OUTPUT));
-	ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_33, GPIO_MODE_OUTPUT));
+    ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT));
+    ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_27, GPIO_MODE_OUTPUT));
+    ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_33, GPIO_MODE_OUTPUT));
 }
