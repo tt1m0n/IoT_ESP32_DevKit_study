b376f19d9dcbc207f476129953ebbdc29f4fe2ee
makovskyi <makovskiy.kpd@gmail.com>
2020-08-17 02:13:32 +0300

    added t03 task: switch on leds with buttons SW1(GPIO39), SW2(GPIO18)

---
+++ t03_Button_SW1_SW2/README.md
@@ -0,0 +1,3 @@
+# DESCRIPTION of Task
+
+Button SW1 must affect on LED1, and button SW2 must affect on LED2.The initial state of the LED is off. One press switches LED on, and another one switches it off.You must avoid switch bounce and be sure that the single button press always works properly
---
+++ t03_Button_SW1_SW2/main/buttons_main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+
+#define LED1 (27)
+#define SW1  (39)
+
+#define LED2 (26)
+#define SW2  (18)
+
+#define HIGH_LEVEL (1)
+#define LOW_LEVEL (0)
+
+#define BTN_UP    (1)
+#define BTN_DOWN  (0)
+
+void button_checking(int button_gpio, int led_gpio, bool* led_on, bool* btn_down)
+{
+    if (gpio_get_level(button_gpio) == BTN_DOWN) {
+        *btn_down = true;
+    } else if (gpio_get_level(button_gpio) == BTN_UP && *btn_down) { // when button in UP position AFTER down position
+        if (*led_on) {
+            *led_on = false;
+        } else {
+            *led_on = true;
+        }
+
+        // update btn_down flag
+        *btn_down = false;
+    }
+
+    if(*led_on){
+        ESP_ERROR_CHECK(gpio_set_level(led_gpio, HIGH_LEVEL));
+    } else {
+        ESP_ERROR_CHECK(gpio_set_level(led_gpio, LOW_LEVEL));
+    }
+}
+
+void app_main()
+{
+    // SW1 and SW2 can have only input mode
+    ESP_ERROR_CHECK(gpio_set_direction(SW1, GPIO_MODE_INPUT));
+    ESP_ERROR_CHECK(gpio_set_direction(SW2, GPIO_MODE_INPUT));
+
+    // set mode output for LED1 and LED2
+    ESP_ERROR_CHECK(gpio_set_direction(LED1, GPIO_MODE_OUTPUT));
+    ESP_ERROR_CHECK(gpio_set_direction(LED2, GPIO_MODE_OUTPUT));
+
+    // initialize helping flags
+    bool led1_on = 0;
+    bool btn1_down = 0;
+    bool led2_on = 0;
+    bool btn2_down = 0;
+
+    while(true) {
+        button_checking(SW1, LED1, &led1_on, &btn1_down);
+        button_checking(SW2, LED2, &led2_on, &btn2_down);
+
+        // it seems not good solution but now i use it for avoiding watchdog errors
+        vTaskDelay(10);
+    }
+}
